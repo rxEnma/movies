5710e4e862611b5dcb763f8fa6973f94adcfd4c4
Festin Bryan <festinbryan7@gmail.com>
2023-10-23 13:55:24 +0200

    Commit image

---
+++ script.js
@@ -0,0 +1,12 @@
+class person {
+    constructor(id, pseudo, picture, age, gender, search, region, description){
+        this.id = id;
+        this.pseudo = pseudo;
+        this.picture = picture;
+        this.age = age;
+        this.gender = gender;
+        this.search = search;
+        this.region = region;
+        this.description = description;
+    }
+}
