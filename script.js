7aecddad64dd6fb058673ef2b7abae361a06b950
bnassim777 <bnassim750@gmail.com>
2023-10-23 14:00:45 +0200

    script et css

--- script.js
+++ script.js
@@ -10,3 +10,44 @@ class person {
         this.description = description;
     }
 }
+// Récupérer les informations du profil
+
+
+const profilElement = document.getElementById("profil"); // Remplacez "profil" par l'ID de votre élément contenant le profil
+
+if (profilElement) {
+    const id = profilElement.getAttribute("data-id");
+    const pseudo = profilElement.querySelector(".pseudo").textContent;
+    const picture = profilElement.querySelector(".picture").getAttribute("src");
+    const age = profilElement.querySelector(".age").textContent;
+    const gender = profilElement.querySelector(".gender").textContent;
+    const search = profilElement.querySelector(".search").textContent;
+    const region = profilElement.querySelector(".region").textContent;
+    const description = profilElement.querySelector(".description").textContent;
+
+    // Stocker les informations dans des cookies
+    document.cookie = `id=${id}`;
+    document.cookie = `pseudo=${pseudo}`;
+    document.cookie = `picture=${picture}`;
+    document.cookie = `age=${age}`;
+    document.cookie = `gender=${gender}`;
+    document.cookie = `search=${search}`;
+    document.cookie = `region=${region}`;
+    document.cookie = `description=${description}`;
+}
+
+// Fonction pour récupérer une valeur de cookie par son nom
+function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split("=");
+        if (cookieName === name) {
+            return decodeURIComponent(cookieValue);
+        }
+    }
+    return null;
+}
+
+// Exemple d'utilisation pour récupérer une information spécifique
+const ageFromCookie = getCookie("age");
+console.log("Âge récupéré depuis le cookie : " + ageFromCookie);
